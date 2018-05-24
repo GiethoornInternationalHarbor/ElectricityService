dba8f8f877c41de4d17c8b597bba71219fecb21c
Marco Havermans <marcohavermans+github@hotmail.nl>
2018-05-24 16:48:37 +0200

    Fixed ship docking and undocking

---
+++ ElectricityService.Core/Models/ShipDockedMessageEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElectricityService.Core.Models
+{
+	public struct ShipDockedMessageEvent
+	{
+		public Guid ShipId { get; set; }
+	}
+}
